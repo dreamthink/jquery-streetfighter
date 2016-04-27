7e583dd501730aa23d03aa8e16a0befd9c14aca3
Emily Chen <emily.c.chen@gmail.com>
2016-04-27 15:56:19 -0400

    Remove hadouken sound

--- js/app.js
+++ js/app.js
@@ -24,9 +24,3 @@ $(document).ready(function() {
 		$(".ryu-ready").show();
 	});
 });
-
-function playHadouken () {
-	$("#hadouken-sound")[0].volume = 0.5;
-	$("#hadouken-sound")[0].load();
-	$("#hadouken-sound")[0].play();
-}
